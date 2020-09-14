144c55bc1d7e8dc26898ab409e549122f8a3820e
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-09-14 15:09:43 +0200

    Added Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 
 import React, {useState} from 'react';
 import { Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,  DrawerActions,
+  getFocusedRouteNameFromRoute, } from '@react-navigation/native';
 import { createStackNavigator  } from '@react-navigation/stack';
- 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import LandingScreen from './screens/Landing';
 import HomeScreen from './screens/Home';
 import SignInScreen from './screens/SignIn';
 import SignUpScreen from './screens/SignUp';
 import PasswordForgetScreen from './screens/PasswordForget';
+
 export type RootStackParamList = {
   Home: undefined;
   Landing:undefined;
   'Sign In': undefined;
   'Sign Up': undefined;
   "Password Forget":undefined
+};
+
+
+const Drawer = createDrawerNavigator();
+ 
+const HomeDrawer = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen
+        name="Password Forget"
+        component={PasswordForgetScreen}
+      />
+    </Drawer.Navigator>
+  );
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
@@ -42,14 +60,23 @@ export default function App() {
     <NavigationContainer>
       <RootStack.Navigator>
       {isAuthenticated ? (
-          <RootStack.Screen 
-            name="Home" 
-            component={HomeScreen} 
-            options={{
+          <RootStack.Screen
+            name="Home"
+            component={HomeDrawer}
+            options={({ route, navigation }) => ({
+              headerTitle: getFocusedRouteNameFromRoute(route),
+              headerLeft: () => (
+                <Button
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.toggleDrawer())
+                  }
+                  title="Menu"
+                />
+              ),
               headerRight: () => (
                 <Button onPress={handleSignOut} title="Sign Out" />
               ),
-            }}
+            })}
           />
         ) : (
         <>
